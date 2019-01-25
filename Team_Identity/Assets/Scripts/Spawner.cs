c11549c398d5e429760cd02d8d42aae827b2042b
chrisworld <christian.walter@outlook.at>
2019-01-25 23:58:18 +0100

    tries with air console

--- Team_Identity/Assets/Scripts/Spawner.cs
+++ Team_Identity/Assets/Scripts/Spawner.cs
@@ -1,18 +1,38 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using NDream.AirConsole;
+using Newtonsoft.Json.Linq;
 
 public class Spawner : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+  public Transform spawn_pos;
+  public GameObject player_prefab;
+  public float spawn_range = 20f; 
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+  void Awake()
+  {
+    AirConsole.instance.onMessage += OnMessage;
+  }
+
+  void OnMessage(int fromDeviceID, JToken data){
+    Debug.Log("message from " + fromDeviceID + "data");
+    if (data["action"] != null && data["action"].ToString().Equals("spawn") ){
+      SpawnPlayer();
     }
+  }
+
+  // Start is called before the first frame update
+  void SpawnPlayer()
+  {
+    Vector3 spawnPosition = new Vector3 (spawn_pos.position.x + Random.Range(-spawn_range, spawn_range), 0.5f, spawn_pos.position.z + Random.Range(-spawn_range, spawn_range));
+    Quaternion spawnRotation = Quaternion.Euler (0f, 0f, 0f);
+    GameObject player = (GameObject)Instantiate (player_prefab, spawnPosition, spawnRotation);
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+      
+  }
 }

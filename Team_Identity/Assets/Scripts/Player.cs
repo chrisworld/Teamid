3a67d239f3a4484a42ad57f0532eff84db0f546f
Thomas Katter <thomas.katter@gmail.com>
2019-01-26 21:01:27 +0100

    dash and stun

--- Team_Identity/Assets/Scripts/Player.cs
+++ Team_Identity/Assets/Scripts/Player.cs
@@ -6,13 +6,20 @@ using UnityEngine.UI;
 
 public class Player : MonoBehaviour
 {
+    //sources
     public string team;
     public Sprite icon;
-    static public Sprite[] baseSprites;
-    static private List<Sprite> spritelist = new List<Sprite>();
+    public GameObject gameManager;
+
+    // attribute
+    public float speed;
+    public float dashDuration;
+    public float dashMultiplier;
+    static float dashCdStat = 2f;
+
     private int points;
     private Vector2 direction;
-    public float speed = 3f;
+
     private Rigidbody2D rb2d;
     public GameObject textPoints;
     private GameObject locatedArea;
@@ -22,91 +29,134 @@ public class Player : MonoBehaviour
     bool movingUp;
     bool movingDown;
     bool isDashing;
+    bool stunned = false;
+    bool blinkOn = false;
+    public bool control = true;
 
     private float nextDash;
-    static float dashCdStat = 2f;
+
     float dashTimer;
     float dashEndtime;
-    public float dashDuration = 1f;
-    public float dashMultiplier = 10f;
+    float stunTimer = 3f;
+    float stunEndtime;
+    float nextBlink;
+    float blinkIntervall = 0.5f;
+    float massReductionStun = 4f;
+
     Vector2 dashDirection;
+
 
     // Start is called before the first frame update
     void Start()
     {
+        nextBlink = Time.time;
+        gameManager = GameObject.FindGameObjectWithTag("Manager");
+        GetRandomIcon();
         rb2d = GetComponent<Rigidbody2D>();
         direction = new Vector2(0, 0);
     }
 
     private void FixedUpdate()
     {
-        direction = Vector2.zero;
-
-        if (movingUp)
-            direction += Vector2.up;
-        if (movingDown)
-            direction += Vector2.down;
-        if (movingLeft)
-            direction += Vector2.left;
-        if (movingRight)
-            direction += Vector2.right;
-
-        if (Input.GetKey("up"))
-            direction += Vector2.up;
-        if (Input.GetKey("down"))
-            direction += Vector2.down;
-        if (Input.GetKey("left"))
-            direction += Vector2.left;
-        if (Input.GetKey("right"))
-            direction += Vector2.right;
-
-        if (direction != Vector2.zero)
+        if (stunned)
         {
-            float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
-            transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
-        }
-
-        //dash attack
-        if (Input.GetKeyDown("space"))
-            Dash();
-
-        if (isDashing)
-        {
-            if(Time.time > dashEndtime)
+            
+            if (Time.time > nextBlink)
             {
-                isDashing = false;
-                transform.localScale = new Vector3(1f, 1f, 1f);
+                nextBlink = Time.time + blinkIntervall;
+                if (blinkOn)
+                {
+                    gameObject.GetComponent<SpriteRenderer>().color = new Color(1f, 1f, 1f, 0.2f);
+                }
+                else
+                {
+                    gameObject.GetComponent<SpriteRenderer>().color = new Color(1f, 1f, 1f, 1f);
+                }
+                blinkOn = !blinkOn;
             }
- 
-        }
-        //move
-        rb2d.AddForce(direction * speed);
-        
+            //End of stun
+            if (Time.time > stunEndtime)
+            {
+                stunned = false;
+                gameObject.GetComponent<Rigidbody2D>().mass = gameObject.GetComponent<Rigidbody2D>().mass * massReductionStun;
+                gameObject.GetComponent<SpriteRenderer>().color = new Color(1f, 1f, 1f, 1f);
+            }
 
- 
+        }
+        else
+        {
+            if (control)
+            {
+                direction = Vector2.zero;
+
+                if (movingUp)
+                    direction += Vector2.up;
+                if (movingDown)
+                    direction += Vector2.down;
+                if (movingLeft)
+                    direction += Vector2.left;
+                if (movingRight)
+                    direction += Vector2.right;
+
+                if (Input.GetKey("up"))
+                    direction += Vector2.up;
+                if (Input.GetKey("down"))
+                    direction += Vector2.down;
+                if (Input.GetKey("left"))
+                    direction += Vector2.left;
+                if (Input.GetKey("right"))
+                    direction += Vector2.right;
+
+                if (direction != Vector2.zero)
+                {
+                    float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+                    transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+                }
+
+                //dash attack
+                if (Input.GetKeyDown("space"))
+                    Dash();
+
+                if (isDashing)
+                {
+                    if (Time.time > dashEndtime)
+                    {
+                        isDashing = false;
+                        transform.localScale = new Vector3(transform.localScale.x, transform.localScale.y * 4f, transform.localScale.z);
+                    }
+
+                }
+                else
+                {
+                    //move
+                    rb2d.AddForce(direction * speed);
+                }
+            }
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
         if (Input.GetButtonUp("Fire1"))
-            if(locatedArea != null && !locatedArea.GetComponent<Area>().team.Equals(team))
-        {
-            Steal(locatedArea);
-        }
-        else if(locatedArea != null && locatedArea.GetComponent<Area>().team.Equals(team))
+            if (locatedArea != null && !locatedArea.GetComponent<Area>().team.Equals(team))
+            {
+                Steal(locatedArea);
+            }
+            else if (locatedArea != null && locatedArea.GetComponent<Area>().team.Equals(team))
             {
                 Deposit(locatedArea);
             }
 
     }
 
-    
+
     private void Dash()
     {
-        if(Time.time > dashTimer) {
+        if (Time.time > dashTimer)
+        {
 
-            transform.localScale = transform.localScale - new Vector3(0, 0.4f, 0);
+            transform.localScale = new Vector3(transform.localScale.x, transform.localScale.y / 4f, transform.localScale.z);
             dashTimer = Time.time + dashCdStat;
             dashEndtime = Time.time + dashDuration;
             isDashing = true;
@@ -122,16 +172,27 @@ public class Player : MonoBehaviour
         Debug.Log("Entered " + locatedArea);
     }
 
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (isDashing)
+        {
+            if (collision.gameObject.tag == "Player")
+            {
+                collision.gameObject.GetComponent<Player>().GetStunned();
+            }
+        }
+    }
+
     private void OnTriggerExit2D(Collider2D collision)
     {
         Debug.Log("Left " + locatedArea);
         locatedArea = null;
-        
+
     }
 
     public void PlayerInput(JToken data)
     {
-        switch(data["action"].ToString())
+        switch (data["action"].ToString())
         {
             case "dpad":
                 switch (data["dpad"]["directionchange"]["key"].ToString())
@@ -172,24 +233,25 @@ public class Player : MonoBehaviour
         }
     }
 
-    static void IconBackToList(Sprite sprite)
+    void IconBackToList(Sprite sprite)
     {
-        spritelist.Add(sprite);
+        gameManager.GetComponent<MainManager>().spritelist.Add(sprite);
     }
 
 
-    private void GetIcon(Sprite icon)
+    private void GetRandomIcon()
     {
-        int rnd = UnityEngine.Random.Range(0, spritelist.Count);
-        gameObject.GetComponent<SpriteRenderer>().sprite = spritelist[rnd];
-        spritelist.RemoveAt(rnd);
+        int rnd = UnityEngine.Random.Range(0, gameManager.GetComponent<MainManager>().spritelist.Count);
+        gameObject.GetComponent<SpriteRenderer>().sprite = gameManager.GetComponent<MainManager>().spritelist[rnd];
+        gameManager.GetComponent<MainManager>().spritelist.RemoveAt(rnd);
     }
 
-    
-
-    private void Move(Vector2 direction)
+    public void GetStunned()
     {
-
+        //Beginn of stun
+        stunned = true;
+        stunEndtime = Time.time + stunTimer;
+        gameObject.GetComponent<Rigidbody2D>().mass = gameObject.GetComponent<Rigidbody2D>().mass / massReductionStun;
     }
 
     private void Dodge()

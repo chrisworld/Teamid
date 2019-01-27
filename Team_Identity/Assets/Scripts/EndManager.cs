8c380e3adc58e6e1ab9c4ac3066517925a836ad6
Soribus <47041295+Soribus@users.noreply.github.com>
2019-01-27 01:36:03 +0100

    Endgame+Condition

---
+++ Team_Identity/Assets/Scripts/EndManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EndManager : MonoBehaviour
+{
+
+    public GameObject Text;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Text.GetComponent<Text>().text = MainManager.winningTeam + " won the game with " + MainManager.winningTeamPoints + " : " + MainManager.losingTeamPoints;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

5b87ef7bce02cabef45b1d806f51e0ec12292ae4
Thomas Katter <thomas.katter@gmail.com>
2019-01-27 12:27:35 +0100

    Endgame restart

--- Team_Identity/Assets/Scripts/EndManager.cs
+++ Team_Identity/Assets/Scripts/EndManager.cs
@@ -2,20 +2,39 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class EndManager : MonoBehaviour
 {
-
+    public static int restartCd = 10;
+    float restartTimer;
+    public GameObject TextTimer;
     public GameObject Text;
     // Start is called before the first frame update
     void Start()
     {
+        StartCoroutine("Timer");
+        Time.timeScale = 1;
+        restartTimer = restartCd;
         Text.GetComponent<Text>().text = MainManager.winningTeam + " won the game with " + MainManager.winningTeamPoints + " : " + MainManager.losingTeamPoints;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        TextTimer.GetComponent<Text>().text = "Restart in " + restartTimer;
+        if(restartTimer<=0)
+        {
+            SceneManager.LoadScene(0);
+        }
+    }
+
+    IEnumerator Timer()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(1);
+            restartTimer--;
+        }
     }
 }
